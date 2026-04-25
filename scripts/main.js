9d86c1b6e627bd924c16c9598a630c266b8c4472
Shajan Jacob <shajanjp@gmail.com>
2026-04-25 23:07:45 +0530

    feat: minor layout and ux changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,85 +1,86 @@
-var morseTextContainer = document.getElementById('morse-text');
-var bodyElement = document.getElementById('main');
+var morseTextContainer = document.getElementById("morse-text");
+var morseSequenceContainer = document.getElementById("morse-sequence");
+var bodyElement = document.getElementById("main");
 var isTrue = true;
-var charBuffer = '';
+var charBuffer = "";
 var mouseDownTime;
 var mouseUpTime;
 var charSet = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
 ];
 
 var morseDataSet = {
-  MCA: '01',
-  MCB: '1000',
-  MCC: '1010',
-  MCD: '100',
-  MCE: '0',
-  MCF: '0010',
-  MCG: '110',
-  MCH: '0000',
-  MCI: '00',
-  MCJ: '0111',
-  MCK: '101',
-  MCL: '0100',
-  MCM: '11',
-  MCN: '10',
-  MCO: '111',
-  MCP: '0110',
-  MCQ: '1101',
-  MCR: '010',
-  MCS: '000',
-  MCT: '1',
-  MCU: '001',
-  MCV: '0001',
-  MCW: '011',
-  MCX: '1001',
-  MCY: '1011',
-  MCZ: '1100',
-  MC0: '11111',
-  MC1: '01111',
-  MC2: '00111',
-  MC3: '00011',
-  MC4: '00001',
-  MC5: '00000',
-  MC6: '10000',
-  MC7: '11000',
-  MC8: '11100',
-  MC9: '11110',
+  MCA: "01",
+  MCB: "1000",
+  MCC: "1010",
+  MCD: "100",
+  MCE: "0",
+  MCF: "0010",
+  MCG: "110",
+  MCH: "0000",
+  MCI: "00",
+  MCJ: "0111",
+  MCK: "101",
+  MCL: "0100",
+  MCM: "11",
+  MCN: "10",
+  MCO: "111",
+  MCP: "0110",
+  MCQ: "1101",
+  MCR: "010",
+  MCS: "000",
+  MCT: "1",
+  MCU: "001",
+  MCV: "0001",
+  MCW: "011",
+  MCX: "1001",
+  MCY: "1011",
+  MCZ: "1100",
+  MC0: "11111",
+  MC1: "01111",
+  MC2: "00111",
+  MC3: "00011",
+  MC4: "00001",
+  MC5: "00000",
+  MC6: "10000",
+  MC7: "11000",
+  MC8: "11100",
+  MC9: "11110",
 };
 
 var newAudioContext = new AudioContext();
@@ -89,7 +90,7 @@ function beep(vol, freq, duration) {
   let u = newAudioContext.createGain();
   v.connect(u);
   v.frequency.value = freq;
-  v.type = 'square';
+  v.type = "square";
   u.connect(newAudioContext.destination);
   u.gain.value = vol * 0.01;
   v.start(newAudioContext.currentTime);
@@ -105,13 +106,18 @@ function playDah() {
 }
 
 function itsError() {
-  bodyElement.classList.remove('success');
-  bodyElement.classList.add('error');
+  bodyElement.classList.remove("success");
+  bodyElement.classList.add("error");
+  setTimeout(function () {
+    charBuffer = "";
+    morseSequenceContainer.innerHTML = "";
+    bodyElement.classList.remove("error");
+  }, 1000);
 }
 
 function itsSuccess() {
-  bodyElement.classList.remove('error');
-  bodyElement.classList.add('success');
+  bodyElement.classList.remove("error");
+  bodyElement.classList.add("success");
 }
 
 function getRandom(max) {
@@ -119,82 +125,77 @@ function getRandom(max) {
 }
 
 function showRandomCharacter() {
-  charBuffer = '';
-  bodyElement.classList.remove('error');
-  bodyElement.classList.remove('success');
+  charBuffer = "";
+  morseSequenceContainer.innerHTML = "";
+  bodyElement.classList.remove("error");
+  bodyElement.classList.remove("success");
   let charSelected = charSet[getRandom(charSet.length)];
-  morseTextContainer.setAttribute('data-text', charSelected);
+  morseTextContainer.setAttribute("data-text", charSelected);
   morseTextContainer.innerHTML = charSelected;
 }
 
 function validateMorse(mc) {
   console.log(mc);
-  let mcc = document.getElementById('morse-text').getAttribute('data-text');
+  let mcc = document.getElementById("morse-text").getAttribute("data-text");
   charBuffer += mc;
+  morseSequenceContainer.innerHTML = charBuffer
+    .split("")
+    .map((c) => (c === "0" ? "." : "-"))
+    .join("");
   if (charBuffer.includes(morseDataSet[`MC${mcc}`])) {
-    console.log('found...');
+    console.log("found...");
     itsSuccess();
-    charBuffer = '';
-    setTimeout(function(){
+    charBuffer = "";
+    setTimeout(function () {
       showRandomCharacter();
-    },1000);
+    }, 1000);
   } else {
-    if(charBuffer.length >= morseDataSet[`MC${mcc}`].length){
+    if (charBuffer.length >= morseDataSet[`MC${mcc}`].length) {
       itsError();
     }
-    console.log('not yet');
+    console.log("not yet");
   }
 }
-
 
 var keyDownTime;
 var keyUpTime;
 var isKeyPressed = false;
 
-(function() {
-  bodyElement.addEventListener("mousedown", function(){
+(function () {
+  bodyElement.addEventListener("mousedown", function () {
     mouseDownTime = Date.now();
   });
-  bodyElement.addEventListener('mouseup', function() {
+  bodyElement.addEventListener("mouseup", function () {
     mouseUpTime = Date.now();
     if (mouseUpTime - mouseDownTime < 100) {
-      validateMorse('0');
+      validateMorse("0");
       playDit();
     } else {
-      validateMorse('1');
+      validateMorse("1");
       playDah();
     }
   });
 
-  window.addEventListener('keydown', function(e) {
-    if (e.key === ' ' || e.code === 'Space') {
+  window.addEventListener("keydown", function (e) {
+    if (isKeyPressed) return;
+
+    if (e.key === ".") {
+      isKeyPressed = true;
+      validateMorse("0");
+      playDit();
+    } else if (e.key === " " || e.code === "Space") {
       e.preventDefault();
-      if (!isKeyPressed) {
-        keyDownTime = Date.now();
-        isKeyPressed = true;
-      }
+      isKeyPressed = true;
+      validateMorse("1");
+      playDah();
     }
   });
-
-  window.addEventListener('keyup', function(e) {
-    if (e.key === ' ' || e.code === 'Space') {
-      e.preventDefault();
-      if (isKeyPressed) {
-        keyUpTime = Date.now();
-        if (keyUpTime - keyDownTime < 100) {
-          validateMorse('0');
-          playDit();
-        } else {
-          validateMorse('1');
-          playDah();
-        }
-        isKeyPressed = false;
-      }
-    }
+  window.addEventListener("keyup", function (e) {
+    isKeyPressed = false;
   });
 
-  setTimeout(function(){
+  setTimeout(function () {
     document.getElementById("instruction").innerHTML = "";
     showRandomCharacter();
-  }, 3000)
+  }, 3000);
 })();
